18295af7b1b4db11ba1f20005089bcd0c20854fa
Rui Santiago <ruisantiagomr@gmail.com>
2021-07-09 11:32:50 +0100

    takewhile rxjs

---
+++ rxjs/take-while/README.md
@@ -0,0 +1,5 @@
+Emit values until provided expression is false.
+
+# Documentation:
+
+https://www.learnrxjs.io/learn-rxjs/operators/filtering/takewhile
---
+++ rxjs/take-while/take-while.js
@@ -0,0 +1,12 @@
+// RxJS v6+
+import { of } from "rxjs";
+import { takeWhile } from "rxjs/operators";
+
+//emit 1,2,3,4,5
+const source$ = of(1, 2, 3, 4, 5);
+
+//allow values until value from source is greater than 4, then complete
+source$
+  .pipe(takeWhile((val) => val <= 4))
+  // log: 1,2,3,4
+  .subscribe((val) => console.log(val));
