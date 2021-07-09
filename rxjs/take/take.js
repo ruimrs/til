daec9582f6e2e014162c2e7d4310a107142de7d0
Rui Santiago <ruisantiagomr@gmail.com>
2021-07-09 11:23:55 +0100

    take example rxjs

---
+++ rxjs/take/take.js
@@ -0,0 +1,10 @@
+// RxJS v6+
+import { interval } from "rxjs";
+import { take } from "rxjs/operators";
+
+//emit value every 1s
+const interval$ = interval(1000);
+//take the first 5 emitted values
+const example = interval$.pipe(take(5));
+//output: 0,1,2,3,4
+const subscribe = example.subscribe((val) => console.log(val));
